2137411a033a2e29ef77f002a5f64b7517fb621e
Andy Weir <andy@ocean-internet.co.uk>
2012-05-03 22:49:03 +0100

    moving files about...

---
+++ lib/Test/TestApp/Config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbCconnectionParams = array(
+    'dbname'   => 'skeleton_test',
+    'user'     => 'skeleton_test',
+    'password' => 'skeleton_test',
+    'host'     => 'localhost',
+    'driver'   => 'pdo_mysql',
+);
